2fafe5562232139fc271d26a9dc9406337b4a863
unknown <myself.suraj0@gmail.com>
2023-06-18 14:26:08 +0530

    password reset fuction added

--- src/Components/Pages/SignIn.jsx
+++ src/Components/Pages/SignIn.jsx
@@ -5,11 +5,13 @@ import AuthContext from "../../Store/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Loader from "../UI/Loader";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isPasswordValid, setIsPasswordValid] = useState(false);
+  const [loader, setLoader] = useState(false);
 
   const authCtx = useContext(AuthContext);
 
@@ -42,7 +44,8 @@ const SignIn = () => {
           returnSecureToken: true,
         }
       );
-      authCtx.isLogin(res.data.idToken);
+      authCtx.isLogin(res.data);
+      setLoader(false);
       toast.success("Login Successfull", {
         position: "top-center",
         autoClose: 5000,
@@ -60,45 +63,83 @@ const SignIn = () => {
     }
   };
 
+  const passwordResetHandler = async () => {
+    setLoader(true);
+    if (email.trim().length === 0) {
+      setLoader(false);
+      return;
+    }
+    try {
+      const res = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyD-vOPcurI7hmCvWd4tS1jCqd71PTwut_M`,
+        {
+          requestType: "PASSWORD_RESET",
+          email: email,
+        }
+      );
+      toast.success("Reset link has been sent to your email", {
+        position: "top-center",
+        autoClose: 5000,
+        theme: "colored",
+      });
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      toast.error("Failed! Please Try Again", {
+        position: "top-center",
+        autoClose: 5000,
+        theme: "colored",
+      });
+    }
+    setLoader(false);
+  };
   return (
-    <div className={styles.container}>
-      <ToastContainer />
-      <h2 className={styles.title}>Sign In</h2>
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Email:</label>
-          <input
-            className={styles.input}
-            type="email"
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label className={styles.label}>Password:</label>
-          <input
-            className={styles.input}
-            type="password"
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-          {isPasswordValid && (
-            <p className={styles["wrong-password"]}>
-              Minimum 8 caracters required
-            </p>
-          )}
-        </div>
-
-        <button className={styles.button} type="submit">
-          Sign In
-        </button>
-      </form>
-      <p onClick={goToSignUpHandler} className={styles["change-auth"]}>
-        Don't have any account? Create New Account
-      </p>
-    </div>
+    <>
+      {loader && <Loader />}
+      <div className={styles.container}>
+        <ToastContainer />
+        <h2 className={styles.title}>Sign In</h2>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.formGroup}>
+            <label className={styles.label}>Email:</label>
+            <input
+              className={styles.input}
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label className={styles.label}>Password:</label>
+            <input
+              className={styles.input}
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+            {isPasswordValid && (
+              <p className={styles["wrong-password"]}>
+                Minimum 8 caracters required
+              </p>
+            )}
+          </div>
+          <p
+            onClick={passwordResetHandler}
+            className={styles["reset-password"]}
+          >
+            Forgot Password?
+          </p>
+          <button className={styles.button} type="submit">
+            Sign In
+          </button>
+        </form>
+        <p onClick={goToSignUpHandler} className={styles["change-auth"]}>
+          Don't have any account? Create New Account
+        </p>
+      </div>
+    </>
   );
 };
 
